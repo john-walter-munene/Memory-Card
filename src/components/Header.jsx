0b01392c9896f9454abb78828c6f411994fcd987
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2025-12-03 14:25:03 +0300

    Add core UI and GIF data handling for memory game .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-function GameHeader() {
-    return (<div>
-        <h1>Soccer Memory Game</h1>
-        
-        <p>Get points on clicking an image but never click on any more than once</p>
+function GameHeader({ scores }) {
+    const { currentScore, bestScore } = scores;
+    return (
+        <div className="game-header">
+            <div className="title-section">
+                <h1>Soccer Memory Game</h1>
+                <p>Get points by clicking images — but never click the same one twice!</p>
+            </div>
 
-        <h3>Score: 0</h3>
-
-        <h3>Best Score: 0</h3>
-    </div>)
+            <div className="score-section">
+                <h3>Score: {currentScore}</h3>
+                <h3>Best Score: {bestScore}</h3>
+            </div>
+        </div>
+    )
 }
 
 export { GameHeader };
--- src/components/MemoryCardGame.jsx
+++ src/components/MemoryCardGame.jsx
@@ -1,11 +1,40 @@
+// Imports in the order: Third party libraries, React Tools, utilities, App specific components.
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { useState, useEffect } from "react";
+import { adaptGIFsToLightWeight } from './utils';
 import { GameHeader } from "./Header";
 import { SoccerGameBoard } from "./GameBoard";
 
 function SoccerMemoryCardGameHouse () {
-    return (<div className="soccer-memory-house">
-        <GameHeader />
-        <SoccerGameBoard />
-    </div>);
+    const [soccerPhotoCards, setSoccerPhotoCards] = useState([]);
+    const [scores, setScores] = useState({ currentScore: 0, bestScore: 0 });
+    // const [restartFlag, setRestartFlag] = useState(false);
+    typeof setScores; 
+
+    useEffect(() => {
+        let ignore = false;
+
+        // Fetch Giphy scoccer images.
+        async function fetchGIFs() {
+            const gifAPI = new GiphyFetch('cdV83dbgRL2VFc2iNoBozLAQFezGJQY6');
+            const { data: soccerGIFs } = await gifAPI.search('american soccer', { limit: 30, type: 'stickers'});
+
+            if (!ignore && soccerGIFs) {
+                const lightSoccerGIFs = adaptGIFsToLightWeight(soccerGIFs);
+                setSoccerPhotoCards(lightSoccerGIFs);
+            }
+        }
+
+        fetchGIFs();
+
+        return () => { ignore = true };
+    }, []);
+
+    return (
+        <div className="soccer-memory-house">
+            <GameHeader scores={scores} />
+            <SoccerGameBoard soccerGIFs={soccerPhotoCards} />
+        </div>);
 }
 
 export { SoccerMemoryCardGameHouse};
